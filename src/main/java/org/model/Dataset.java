0aa9e4563d00e45928b147a33a858e4cb25463d7
Rafael Costa <44804@tjmt.jus.br>
2023-10-21 18:56:59 -0400

    feat: Created Dataset Factory

---
+++ src/main/java/org/model/Dataset.java
@@ -0,0 +1,8 @@
+package org.model;
+
+public class Dataset extends Simpsons {
+
+        public Dataset(int age, String genre, String height, double weight, String topClothesColor, String bottomClothesColor, String shoesColor, String hairColor) {
+            super(age, genre, height, weight, topClothesColor, bottomClothesColor, shoesColor, hairColor);
+        }
+}
