819df10f5853f06a02eedea3d440dd1486fc3753
Rafael Costa <44804@tjmt.jus.br>
2023-10-22 21:45:00 -0400

    feat: add Multilayer Perceptron function

--- src/main/java/org/main/DecisionTree.java
+++ src/main/java/org/main/DecisionTree.java
@@ -1,7 +1,12 @@
 package org.main;
 
+import weka.attributeSelection.ASEvaluation;
+import weka.attributeSelection.AttributeEvaluator;
+import weka.attributeSelection.ClassifierAttributeEval;
+import weka.attributeSelection.GainRatioAttributeEval;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
@@ -24,10 +29,12 @@ public class DecisionTree {
         System.out.println("************************** J48 *************************");
         /** Classifier here is Linear Regression */
         Classifier classifier = new J48();
+        Classifier mpClassifier = new MultilayerPerceptron();
 
-        //J48,Id3
+        //J48,MultilayerPerceptron
         /** */
         classifier.buildClassifier(trainingDataSet);
+        mpClassifier.buildClassifier(testingDataSet);
         /**
          * train the alogorithm with the training data and evaluate the
          * algorithm with testing data
@@ -43,5 +50,16 @@ public class DecisionTree {
         System.out.println(classifier);
         System.out.println(eval.toMatrixString());
         System.out.println(eval.toClassDetailsString());
+
+        Evaluation mpEval = new Evaluation(trainingDataSet);
+        mpEval.evaluateModel(mpClassifier, testingDataSet);
+        /** Print the algorithm summary */
+        System.out.println("** Decision Tress Evaluation with Datasets **");
+        System.out.println(mpEval.toSummaryString());
+        System.out.println(mpEval.SFSchemeEntropy());
+        System.out.print(" the expression for the input data as per alogorithm is ");
+        System.out.println(mpClassifier);
+        System.out.println(mpEval.toMatrixString());
+        System.out.println(mpEval.toClassDetailsString());
     }
 }
